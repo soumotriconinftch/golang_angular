5dcecc91784587a642e24035b2f3a31e7ddfd050
Soumo Saha <soumosaha4327@gmail.com>
2025-12-18 08:34:36 +0530

    fix: cors.go

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,13 +1,24 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-var origins = "localhost, google.com, hello"
+// var origins = "localhost, google.com, hello"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", origins)
+		origin := r.Header.Get("Origin")
+		// Allow any localhost origin for development
+		// In production, checking against a specific whitelist is better
+		log.Println(origin)
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Upgrade-Insecure-Requests")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
